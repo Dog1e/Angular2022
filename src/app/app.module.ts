e8a214bd85a8be70163be91bdb52291261ad99a4
Dog1e <73541429+Dog1e@users.noreply.github.com>
2022-09-22 15:08:52 +0200

    2 files modified, 12 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
+import { Routes, RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+import { MenuComponent } from './menu/menu.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { ContactComponent } from './contact/contact.component';
+
+const routes: Routes = [
+  { path: '', component: MenuComponent },
+  { path: 'Menu', component: MenuComponent },
+  { path: 'Gallery', component: GalleryComponent },
+  { path: 'Contact', component: ContactComponent },
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  imports: [BrowserModule, FormsModule, RouterModule.forRoot(routes), ReactiveFormsModule],
+  declarations: [AppComponent, HelloComponent, MenuComponent, GalleryComponent, PageNotFoundComponent, ContactComponent],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {}
